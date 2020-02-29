17d725277f13a704346ae3348f3c7ed439fbf5c6
skye <skye@hostone.com.au>
2020-02-29 20:53:04 +0800

    AAdded additional data validation unit tests

--- UnitTests/Tests/ValidationTest.php
+++ UnitTests/Tests/ValidationTest.php
@@ -35,14 +35,138 @@
 		}
 
 		public function testNullDataAllowed(): void {
-			$value = DataValidator::ForValue(null, true)->value();
+			$value = DataValidator::ForValue(null, true)
+								  ->value();
 
 			$this->assertNull($value);
 		}
 
 		public function testNullDataNotAllowed(): void {
 			$this->expectException(Exception::class);
-			DataValidator::ForValue(null, false)->value();
+			DataValidator::ForValue(null, false)
+						 ->value();
+		}
+
+		public function testValidInteger(): void {
+			$int = 5;
+
+			$value = DataValidator::ForValue($int)
+								  ->int()
+								  ->value();
+
+			$this->assertEquals($value, $int);
+		}
+
+		public function testIntegerWithinRange(): void {
+			$int = 5;
+
+			$value = DataValidator::ForValue($int)
+								  ->int(4, 6)
+								  ->value();
+
+			$this->assertEquals($int, $value);
+		}
+
+		public function testIntegerBelowRange(): void {
+			$int = 3;
+
+			$this->expectException(Exception::class);
+			DataValidator::ForValue($int)
+						 ->int(4, 6)
+						 ->value();
+		}
+
+		public function testIntegerAboveRange(): void {
+			$int = 7;
+
+			$this->expectException(Exception::class);
+			DataValidator::ForValue($int)
+						 ->int(4, 6)
+						 ->value();
+		}
+
+		public function testInvalidInteger(): void {
+			$int = 7.5;
+
+			$this->expectException(Exception::class);
+			DataValidator::ForValue($int)
+						 ->int(4, 6)
+						 ->value();
+		}
+
+		public function testValidFloat(): void {
+			$float = 5.5;
+
+			$value = DataValidator::ForValue($float)
+								  ->float()
+								  ->value();
+
+			$this->assertEquals($value, $float);
+		}
+
+		public function testFloatWithinRange(): void {
+			$float = 5.5;
+
+			$value = DataValidator::ForValue($float)
+								  ->float(5, 6)
+								  ->value();
+
+			$this->assertEquals($float, $value);
+		}
+
+		public function testFloatBelowRange(): void {
+			$float = 5.5;
+
+			$this->expectException(Exception::class);
+			DataValidator::ForValue($float)
+						 ->float(5.6, 6)
+						 ->value();
+		}
+
+		public function testFloatAboveRange(): void {
+			$float = 5.5;
+
+			$this->expectException(Exception::class);
+			DataValidator::ForValue($float)
+						 ->float(4, 5.4)
+						 ->value();
+		}
+
+		public function testValidEmailAddress(): void {
+			$email = 'alias+test@email.domain.com';
+
+			$value = DataValidator::ForValue($email)
+								  ->string()
+								  ->emailAddress()
+								  ->value();
+
+			$this->assertEquals($value, $email);
+		}
+
+		public function testInvalidEmailAddress(): void {
+			$email = '@email.';
+
+			$this->expectException(Exception::class);
+			DataValidator::ForValue($email)
+						 ->string()
+						 ->emailAddress()
+						 ->value();
+		}
+
+		public function testValidJsonObject(): void {
+			$object = [
+				'key' => 'val'
+			];
+
+			$json = json_encode($object, JSON_THROW_ON_ERROR, 512);
+
+			$value = DataValidator::ForValue($json)
+								  ->jsonObject()
+								  ->value();
+
+			$this->assertIsObject($value);
+			$this->assertObjectHasAttribute('key', $value);
+			$this->assertEquals($object['key'],$value->key);
 		}
 
 	}
