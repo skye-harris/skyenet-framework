bad00c3de68c48a8a18e42d7410159f4adac0501
skye <skye@hostone.com.au>
2020-03-01 11:19:23 +0800

    Does not need to call its parent

--- UnitTests/UnitTest.php
+++ UnitTests/UnitTest.php
@@ -11,17 +11,12 @@
 	use Skyenet\Database\MySQL\Connection;
 
 	abstract class UnitTest extends TestCase {
-
 		protected function setUp(): void {
-			parent::setUp();
-
 			$sql = Connection::getInstance();
 			$sql->beginTransaction();
 		}
 
 		protected function tearDown(): void {
-			parent::tearDown();
-
 			$sql = Connection::getInstance();
 			$sql->rollbackTransaction();
 		}
